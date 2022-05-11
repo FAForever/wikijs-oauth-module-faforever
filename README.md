139f02426a3572e89aa55ad56519c30275315298
Sheikah <Sheikah450@gmail.com>
2022-05-11 00:43:07 -0400

    added authentication module

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wikijs-oauth-module-faforever
 This is a module to allow for oauth login using the faf hydra oauth provider
+
+To use this with a wikijs installation you must add the faf-hydra folder to the /server/modules/authentication folder
+Once added to the mentioned folder a new strategy named FAF Ory Hydra Login becomes available. Enable this strategy and create
+and oauth client in hydra with the proper redirect_uri to use it for single sign on.
+
+This plugin works by retrieving an access token from hydra and then using it to query the api and get the user info.
+This user info is then used to either populate or update the user in the wiki. Users are uniquely mapped to the api through their
+player ids. So even when username or email address changes occur the wiki information will be updated accordingly on login.
+
---
+++ faf-hydra/authentication.js
@@ -0,0 +1,48 @@
+const OidcStrategy = require('passport-openidconnect')
+const request = require('request')
+
+module.exports = {
+  init (passport, conf) {
+    passport.use(conf.key,
+      new OidcStrategy({
+        issuer: conf.oauthBaseUrl + '/',
+        tokenURL: conf.oauthBaseUrl + '/oauth2/token',
+        authorizationURL: conf.oauthBaseUrl + '/oauth2/auth',
+        userInfoURL: conf.oauthBaseUrl + '/userinfo?schema=openid',
+        clientID: conf.clientId,
+        clientSecret: conf.clientSecret,
+        callbackURL: conf.callbackURL,
+        scope: ['openid', 'public_profile'],
+        passReqToCallback: true
+      },
+      function (req, iss, sub, profile, jwtClaims, accessToken, refreshToken, params, verified) {
+        const providerKey = req.params.strategy
+        request.get(
+          {
+            url: conf.apiUrl + '/me',
+            headers: {'Authorization': 'Bearer ' + accessToken}
+          },
+          function (e, r, body) {
+            if (r.statusCode !== 200) {
+              console.log('Auth failure: ' + r.statusCode)
+
+              return verified(null)
+            }
+            let user = JSON.parse(body)
+            console.log(user)
+            let accountPromise = WIKI.models.users.processProfile({providerKey: providerKey, profile: {id: user.data.attributes.userId, email: user.data.attributes.email, displayName: user.data.attributes.userName}})
+            accountPromise.then(account => {verified(null, account)})
+          }
+        )
+      }
+      )
+    )
+  },
+  logout (conf) {
+    if (!conf.logoutURL) {
+      return '/'
+    } else {
+      return conf.logoutURL
+    }
+  }
+}
